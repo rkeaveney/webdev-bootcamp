e4b01a1a8360d6d2c2197adf3ceefce71c5a0b1d
rkeaveney <ronan.keaveney@gmail.com>
2023-12-08 23:37:24 +0000

    Keeper app part 2

--- react/keeper/src/components/App.jsx
+++ react/keeper/src/components/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import notes from '../notes'
 
 function App() {
 	return(
 		<div>
 			<Header />
-			<Note />
+			{notes.map((x) => (<Note key={x.id} title={x.title} content={x.content}/>))}
 			<Footer />
 		</div>
 	);
